a65fd254a2538ebf2caa6984686a0705dd69c965
rrs6 <rrs6@cin.ufpe.br>
2024-09-10 10:30:29 -0300

    feat: adding .env and dotenv config in app file.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express();
-const port = Number(process.env.PORT);
+const port = Number(process.env.SERVER_PORT);
 
 app.listen(port, () => {
     console.log("Server is running on port "+ port);
